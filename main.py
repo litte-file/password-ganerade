7cf04b99ae70772399fb1c510bdea7cfbbddee4c
little-file <fakelittleaqqff@protonmail.com>
2023-03-05 11:25:33 +0300

    added ultimate passworld

--- main.py
+++ main.py
@@ -1,18 +1,48 @@
 import random
 
+print("hello")
+def classic_password():
+    u = "QWERTYUIOPASDFGHJKLZXCVBNM"
 
-u = "QWERTYUIOPASDFGHJKLZXCVBNM"
+    d = "qazwsxedcrfvtgbyhnujmikol"
 
-d = "qazwsxedcrfvtgbyhnujmikol"
+    s = "!@#$%^&*()_+-=`~[{]}\|;:/?.>,<"
 
-s = "!@#$%^&*()_+-=`~[{]}\|;:/?.>,<"
+    n = "0123456789"
 
-n = "0123456789"
+    all=u+d+s+n
 
-all=u+d+s+n
+    l = int(input("number pls: "))
 
-l = int(input("number pls: "))
+    p = "".join(random.sample(all,l))
 
-p = "".join(random.sample(all,l))
+    print("your password: ",p)
+    x = False
+def unlimited(x = True):
+    u = "QWERTYUIOPASDFGHJKLZXCVBNM"
 
-print("your password: ",p)
+    d = "qazwsxedcrfvtgbyhnujmikol"
+
+    s = "!@#$%^&*()_+-=`~[{]}\|;:/?.>,<"
+
+    n = "0123456789"
+
+    all=u+d+s+n
+
+    l = int(input("number pls: "))
+    while x == True:
+        p = "".join(random.sample(all,l))
+
+        print("your password: ",p)
+
+x = True
+
+while x == True:
+    print("""    [1]classic passworld
+    [2]unlimited random password
+    [3]quit""")
+    c = int(input("pls only number: "))
+    if c == 1:
+        classic_password()
+    elif c == 2:
+        unlimited()
